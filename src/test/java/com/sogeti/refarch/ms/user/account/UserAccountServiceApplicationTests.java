c9051ac1e1dca0944035569347656431025916c4
mrigank-kishore <mrigank.online@gmail.com>
2017-03-09 23:15:49 +0530

    Add files via upload
    initial commit

---
+++ src/test/java/com/sogeti/refarch/ms/user/account/UserAccountServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sogeti.refarch.ms.user.account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserAccountServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
